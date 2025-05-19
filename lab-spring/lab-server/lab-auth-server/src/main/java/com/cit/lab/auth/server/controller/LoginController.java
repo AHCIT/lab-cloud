91748f43166cfe84fd750c28a80eeb11891d3754
zhouli <1528472216@qq.com>
2025-05-19 21:48:57 +0800

    feat 登录接口定义

---
+++ lab-spring/lab-server/lab-auth-server/src/main/java/com/cit/lab/auth/server/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.cit.lab.auth.server.controller;
+
+/**
+ * @Author: Richard
+ * @Description: LoginController
+ * @CreateDate: 2025/5/19 21:48
+ * @UpdateUser: zhouli
+ * @UpdateDate: 2025/5/19 21:48
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class LoginController {
+}
