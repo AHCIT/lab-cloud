16acae9e209586042fabf1c21b1b9fa281042b24
zhouli <1528472216@qq.com>
2025-07-08 22:45:45 +0800

    feat authentic接口定义

--- lab-spring/lab-server/lab-auth-server/src/main/java/com/cit/lab/auth/server/controller/LoginController.java
+++ lab-spring/lab-server/lab-auth-server/src/main/java/com/cit/lab/auth/server/controller/LoginController.java
@@ -10,4 +10,53 @@ package com.cit.lab.auth.server.controller;
  * @Version: 1.0
  */
 public class LoginController {
+    /**
+     * 登录
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    public Object login(String username, String password) {
+        return null;
+    }
+    /**
+     * 登出
+     *
+     * @return
+     */
+    public Object logout() {
+        return null;
+    }
+    /**
+     * 获取当前登录用户信息
+     *
+     * @return
+     */
+    public Object getCurrentUserInfo() {
+        return null;
+    }
+    /**
+     * 获取当前登录用户权限
+     *
+     * @return
+     */
+    public Object getCurrentUserPermission() {
+        return null;
+    }
+    /**
+     * 刷新token
+     *
+     * @return
+     */
+    public Object refreshToken() {
+        return null;
+    }
+    /**
+     * 交换第三方token
+     *
+     * @return
+     */
+    public Object exchangeThirdPartyToken() {
+        return null;    }
 }
